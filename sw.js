0b4c5c0db531943342f26c96d9355792769528f9
suffeleersdavy <suffeleersdavy@gmail.com>
2026-01-21 10:23:12 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "denkapp-v1";
+const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
